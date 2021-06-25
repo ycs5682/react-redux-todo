6db04018a8570c7fabf371a1039927667365a084
유창선 <ycs5682@gmail.com>
2021-06-25 11:39:41 +0900

    TodoItemコンポーネント作成

--- src/components/todoList.jsx
+++ src/components/todoList.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
+import TodoItem from './todoItem';
 
 const TodoListBlock = styled.div`
   flex: 1;
   padding: 20px 32px;
   padding-bottom: 48px;
   overflow-y: auto;
-  background: gray; 
 `;
 
 const TodoList = () => {
-  return <TodoListBlock>TodoList</TodoListBlock>;
+  return (
+    <TodoListBlock>
+      <TodoItem text='プロジェクト作成' done={true} />
+      <TodoItem text='コンポーネント作成' done={true} />
+      <TodoItem text='Rudux作成' done={false} />
+      <TodoItem text='機能実装' done={false} />
+    </TodoListBlock>
+  );
 };
 
 export default TodoList;
